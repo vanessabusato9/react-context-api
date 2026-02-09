2889f9143824e68802e31fb2afd94f0d0c95b028
vanessa <vane.busato9@gmail.com>
2026-02-09 20:00:06 +0100

    inserito import BudgetProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
+import { BudgetProvider } from "../contexts/BudgetContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Homepage from '../pages/Homepage'
 import AboutUs from "../pages/AboutUs"
 import Products from '../pages/Products'
-
 import DefaultLayout from './layout/DefaultLayout'
 
-import { BudgetProvider } from "../contexts/BudgetContext";
 
 
 function App() {
 
-  return <BrowserRouter>
-    <Routes>
-      <Route Component={DefaultLayout}>
-        <Route path="/" Component={Homepage} />
-        <Route path="/about" Component={AboutUs} />
-        <Route path="/products" Component={Products} />
-      </Route>
-    </Routes>
-  </BrowserRouter >;
+  return <BudgetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={Homepage} />
+          <Route path="/about" Component={AboutUs} />
+          <Route path="/products" Component={Products} />
+        </Route>
+      </Routes>
+    </BrowserRouter >
+  </BudgetProvider>;
 
 
 
